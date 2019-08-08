424aac3d9705b6b458dcf24317585bc703cf97a1
Kevin Lin <kevinptt0323@gmail.com>
2019-08-08 15:49:17 +0800

    expose ptt object in development mode

--- src/actions/connect.js
+++ src/actions/connect.js
@@ -4,6 +4,9 @@ export const CONNECTION = 'CONNECTION';
 const connect = (pttConfig = {}) => dispatch => {
   dispatch({ type: CONNECTION, status: 'connecting' });
   const ptt = new PTT(pttConfig);
+  if (process.env.NODE_ENV !== 'production') {
+    window.ptt = ptt;
+  }
   ptt.once('connect', () => {
     dispatch({ type: CONNECTION, status: 'connected', ptt: ptt});
   });
